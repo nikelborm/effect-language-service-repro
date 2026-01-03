243f4f38d4911ced2b91df6cb72a46c41c1ce30d
nikelborm <evadev@duck.com>
2026-01-03 10:01:12 +0300

    quick save: Sat Jan  3 10:01:12 MSK 2026

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import * as Effect from "effect/Effect"
 import * as Function from 'effect/Function'
 import * as Fn from 'effect/Function'
 import { pipe } from 'effect/Function'
@@ -6,4 +5,3 @@ import { pipe } from 'effect/Function'
 const good = pipe('asd')
 const bad1 = Function.pipe('asd')
 const bad2 = Fn.pipe('asd')
-// Effect.gen(function*(){})
